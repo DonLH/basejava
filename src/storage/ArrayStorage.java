82149edd9cf2f24234696cc88c2944be5477449d
donlh <donlh@yandex.ru>
2022-02-16 12:46:32 +0300

    Refactor ArrayStorage.clear() method

--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -12,9 +12,7 @@ public class ArrayStorage {
     int size = 0;
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size - 1, null);
         size = 0;
     }
 
